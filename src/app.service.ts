2e5461a422cbe484e7948bcc6d817d2afc120f7c
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-08-18 17:49:25 -0500

    year

--- src/app.service.ts
+++ src/app.service.ts
@@ -13,6 +13,6 @@ export class AppService {
   }
   
   getHello(): string {
-    return '<footer><a href="/api">https://backend.subvind.com/api</a><br /><br /><p style="margin: 0;">backend.subvind.com © 2023.</p> <p style="margin: 0;">powered by <a href="https://subvind.com" class="svelte-8o1gnw">subvind</a></p></footer>';
+    return `<footer><a href="/api">https://backend.subvind.com/api</a><br /><br /><p style="margin: 0;">backend.subvind.com © ${new Date().getFullYear()}.</p> <p style="margin: 0;">powered by <a href="https://subvind.com" class="svelte-8o1gnw">subvind</a></p></footer>`;
   }
 }
