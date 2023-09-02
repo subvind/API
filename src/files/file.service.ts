04854bc97a6b41292f13f705b0aa300ecffce784
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-09-01 23:50:02 -0500

    uploads 2

--- src/files/file.service.ts
+++ src/files/file.service.ts
@@ -32,7 +32,7 @@ export class FileService {
     const minioClient = new Minio.Client({
       endPoint: process.env.MINIO_HOST, // Replace with your MinIO host
       port: 80, // Replace with your MinIO port
-      useSSL: true, // Set to true if you're using SSL
+      useSSL: false, // Set to true if you're using SSL
       accessKey: process.env.MINIO_ACCESS_KEY, // Replace with your MinIO access key
       secretKey: process.env.MINIO_SECRET_KEY, // Replace with your MinIO secret key
     });
