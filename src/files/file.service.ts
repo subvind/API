127d5aae4b3e8c4402da86eca6b37b17f2114192
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-09-02 14:31:28 -0500

    uploads 23

--- src/files/file.service.ts
+++ src/files/file.service.ts
@@ -67,6 +67,7 @@ export class FileService {
         {
           "Sid": "PublicReadGetObject",
           "Effect": "Allow",
+          "Principal": "*",
           "Action": "s3:GetObject",
           "Resource": `arn:aws:s3:::${bucketName}/*`
         }
