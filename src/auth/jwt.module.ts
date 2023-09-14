747af277c76ba9591c192e7538ddf0435bd3a009
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-09-14 17:49:16 -0500

    expire token in seconds 3

--- src/auth/jwt.module.ts
+++ src/auth/jwt.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 
-const expiresInInSeconds = 356 * 24 * 60 * 60; // 356 days in seconds
-
 @Module({
   imports: [
     JwtModule.registerAsync({
@@ -11,7 +9,7 @@ const expiresInInSeconds = 356 * 24 * 60 * 60; // 356 days in seconds
       useFactory: async () => ({
         secret: process.env.JWT_SECRET,
         signOptions: { 
-          expiresIn: expiresInInSeconds
+          expiresIn: "365 days"
         }
       }),
     }),
