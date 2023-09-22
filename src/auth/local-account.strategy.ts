eb9ae8d82a6e9462f2bc0fcc580d892a40938d6b
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-09-22 17:18:45 -0500

    account auth 8

--- src/auth/local-account.strategy.ts
+++ src/auth/local-account.strategy.ts
@@ -6,12 +6,18 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class LocalAccountStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super({ usernameField: 'email', organizationField: 'organizationId' });
+    super({ 
+      usernameField: 'email',
+      passReqToCallback: true
+    });
   }
 
-  async validate(email: string, password: string, organizationId: string): Promise<any> {
-    console.log('LocalAccountStrategy', email, password, organizationId)
-    const user = await this.authService.validateAccount(email, password, organizationId);
+  async validate(request, email: string, password: string): Promise<any> {
+    // Access the request object, e.g., request.user for user information
+    console.log('LocalAccountStrategy request:', JSON.stringify(request));
+
+    console.log('LocalAccountStrategy', email, password)
+    const user = await this.authService.validateAccount(email, password, request.organizationId);
     if (!user) {
       throw new UnauthorizedException();
     }
