050debb3e333a25d1f1a68d534001890a5fa34a8
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-09-30 14:58:39 -0500

    Headless API

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,8 @@ async function bootstrap() {
 
   // Create a Swagger document builder
   const options = new DocumentBuilder()
-    .setTitle('Headless ERP')
-    .setDescription('Enterprise Resource Planning backend API')
+    .setTitle('Headless API')
+    .setDescription(`We understand that your business is unique, and your technology should reflect that. That's why we offer a fully customizable solution that empowers you to mold your digital environment to align perfectly with your brand and objectives. Whether it's the user interface, workflow automation, or data integrations, you have the freedom to adapt and evolve your platform as your business evolves, ensuring it remains a true reflection of your vision and goals. With full customization at your fingertips, you're in control of your digital destiny.`)
     .setVersion('1.0')
     .build();
 
