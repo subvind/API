da24bf460ffa38b5942961e04181202020fd645d
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-10-08 20:10:30 -0500

    analytics amqp 3

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,7 @@
-import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  @RabbitSubscribe({
-    exchange: 'exchange1',
-    routingKey: 'subscribe-route',
-    queue: 'subscribe-queue',
-  })
-  public async pubSubHandler(msg: {}) {
-    console.log(`Received message: ${JSON.stringify(msg)}`);
-  }
-  
   getHello(): string {
     return `<footer><a href="/swagger">https://api.subvind.com/swagger</a><br /><br /><p style="margin: 0;">api.subvind.com © ${new Date().getFullYear()}.</p> <p style="margin: 0;">powered by <a href="https://subvind.com" class="svelte-8o1gnw">subvind</a></p></footer>`;
   }
